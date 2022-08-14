e1ed2a65834a30d4393c89403e59a5f188f6bf27
Dan <110688933+Zo-Lar@users.noreply.github.com>
2022-08-14 14:49:23 +0300

    Add Adaptive

--- js/main.js
+++ js/main.js
@@ -7,4 +7,11 @@ $(function() {
     autoplaySpeed: 2000,
   });
 
+  $(".menu, bike__column").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
 });
